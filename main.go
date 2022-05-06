c99d0a931a6d96af1196f0102d42b5afa12b488a
KingAnointing <adetoyeanointing15@gmail.com>
2022-05-06 20:44:07 +0100

    removed hello world and create welcome greeting

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	conferenceName := "King Conference"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("Welcome to %v \n", conferenceName)
+	fmt.Printf("We have %v tickets and %v tickets available for the %v \n",conferenceTickets,remainingTickets,conferenceName )
+
 }
