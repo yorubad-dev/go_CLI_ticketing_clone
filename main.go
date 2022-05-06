0941503cfe9c8dd3113730a2dfbb5e421c3903b9
KingAnointing <adetoyeanointing15@gmail.com>
2022-05-06 22:12:03 +0100

    created else condition for valid input and added a for loop

--- main.go
+++ main.go
@@ -13,33 +13,48 @@ func main() {
 	// greeting
 	fmt.Printf("Welcome to %v \n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v tickets available for the %v \n", conferenceTickets, remainingTickets, conferenceName)
+	for {
+		// user input
+		var firstName string
+		var lastName string
+		var email string
+		var userTicket uint
 
-	// user input
-	var firstName string
-	var lastName string
-	var email string
-	var userTicket uint
+		fmt.Println("Enter First Name: ")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter First Name: ")
-	fmt.Scan(&firstName)
+		fmt.Println("Enter last Name: ")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter last Name: ")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter Email Address: ")
+		fmt.Scan(&email)
 
-	fmt.Println("Enter Email Address: ")
-	fmt.Scan(&email)
+		fmt.Println("Enter Number of Tickets: ")
+		fmt.Scan(&userTicket)
 
-	fmt.Println("Enter Number of Tickets: ")
-	fmt.Scan(&userTicket)
+		validNames := len(firstName) >= 2 && len(lastName) >= 2
+		validEmail := strings.Contains(email, "@")
+		validTickets := remainingTickets > 0 && userTicket <= remainingTickets
 
-	validNames := len(firstName) >= 2 && len(lastName) >= 2
-	validEmail := strings.Contains("email", "@")
-	validTickets := remainingTickets > 0 && userTicket <= remainingTickets
+		if validNames && validEmail && validTickets {
+			fmt.Printf("Thank you %v %v for purchasing %v tickets, confirmation will be sent to %v \n", firstName, lastName, userTicket, email)
+			remainingTickets = remainingTickets - userTicket
+			fmt.Printf("%v purchased, %v tickets remaining \n", userTicket, remainingTickets)
+			if remainingTickets == 0 {
+				fmt.Printf("%v has been booked out\n", conferenceName)
+				break
+			}
+		} else {
+			if !validNames {
+				fmt.Println("Either first name or last name are too short")
+			}
+			if !validEmail {
+				fmt.Println("Email does not contain @... it is invalid")
+			}
+			if !validTickets {
+				println("Ticket entered is invalid")
+			}
+		}
 
-	if validNames && validEmail && validTickets {
-		fmt.Printf("Thank you %v %v for purchasing %v tickets, confirmation will be sent to %v \n", firstName, lastName, userTicket, email)
-		remainingTickets = remainingTickets - userTicket
-		fmt.Printf("%v purchased, %v tickets remaining \n", userTicket, remainingTickets)
 	}
-
 }
