6eaa18b3ef9e231db0a4321523a8c13db5d0768b
KingAnointing <adetoyeanointing15@gmail.com>
2022-05-06 21:02:28 +0100

    added user input in main function

--- main.go
+++ main.go
@@ -4,10 +4,29 @@ import "fmt"
 
 func main() {
 	conferenceName := "King Conference"
-	const conferenceTickets = 50
+	const conferenceTickets int = 50
 	var remainingTickets uint = 50
-
+	// greeting
 	fmt.Printf("Welcome to %v \n", conferenceName)
-	fmt.Printf("We have %v tickets and %v tickets available for the %v \n",conferenceTickets,remainingTickets,conferenceName )
+	fmt.Printf("We have a total of %v tickets and %v tickets available for the %v \n",conferenceTickets,remainingTickets,conferenceName )
 
+	// user input
+	var firstName string
+	var lastName string
+	var email string
+	var userTicket uint
+
+	fmt.Println("Enter First Name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter last Name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter Email Address: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter Number of Tickets: ")
+	fmt.Scan(&userTicket)
+
+	fmt.Printf("Thank you %v %v for purchasing %v tickets, confirmation will be sent to %v \n", firstName, lastName,userTicket, email)
 }
